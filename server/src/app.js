e797c409718743edd4ddd206b133dc255b7697a6
Sanskriti Lamsal <sanskritilamsal@hst000147876um.wifi02.olemiss.edu>
2019-09-03 17:15:31 -0500

    setting up server

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+console.log("Hello")
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+app.get('/status', (req, res)=> {
+
+    res.send({
+        message: 'hello World!'
+    })
+
+})
+
+
+app.listen(process.env.PORT || 8081);
